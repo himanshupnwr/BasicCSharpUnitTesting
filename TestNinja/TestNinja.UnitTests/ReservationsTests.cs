225eeb28cb50f894940dd3ea14e82af009bfbde0
Himanshu Panwar <himanshupanwar001@gmail.com>
2019-05-03 02:07:23 +0530

    Day One Learning - Writing Simple Unit Tests

---
+++ TestNinja/TestNinja.UnitTests/ReservationsTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationsTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User {IsAdmin = true});
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+        {
+            //Arrange
+            var user = new User();
+            var reservation = new Reservation {MadeBy = user};
+
+            //Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnsFalse()
+        {
+            //Arrange
+            var reservation = new Reservation {MadeBy = new User()};
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
